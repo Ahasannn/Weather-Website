c1099f203cc3e7c002b5093aa5d4eee5190413d6
Ahasan Kabir <ahasankabir@Ahasans-MacBook-Pro.local>
2023-06-10 06:57:28 +0600

    Add humidity information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -17,8 +17,9 @@ const forecast = (latitude,longitude,callback) => {
             callback('Unable to find location',undefined)
 
         }else{
-
-            callback(undefined,body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+ " degrees out. It feels like "+body.current.feelslike+" degrees out.")
+             
+            console.log(body.current)
+            callback(undefined,body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+ " degrees out. It feels like "+body.current.feelslike+" degrees out. The humadity is "+body.current.humidity+"%.")
         }
 
     })

1a28219a54df87d7a395e1fe67cb0f5fd6b7e4c8
Ahasan Kabir <ahasankabir@Ahasans-MacBook-Pro.local>
2023-06-10 06:30:39 +0600

    Setup app for deployment

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ console.log(__dirname)
 console.log(path.join(__dirname,'../public'))
 
 const app = express()
+const port = process.env.PORT || 3000
 
 
 //Define paths for express config
@@ -130,8 +131,8 @@ app.get('*',(req,res)=>{
 
 })
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000.')
+app.listen(port,()=>{
+    console.log('Server is up on port '+port)
 })
 
 

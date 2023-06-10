7b9ed1f9e7f1af3497cf7fe23735a4f7cc46e134
Ahasan Kabir <ahasankabir@Ahasans-MacBook-Pro.local>
2023-06-10 07:37:13 +0600

    Updating the help page

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ app.get('/about',(req,res)=>{
 
 app.get('/help',(req,res)=>{
     res.render('help',{
-        helpText: 'This page is for help',
+        helpText: 'For any kind of query, contact me at ahasankabir146@gmail.com',
         title: 'Help',
         name : 'Ahasan Kabir'
     })
